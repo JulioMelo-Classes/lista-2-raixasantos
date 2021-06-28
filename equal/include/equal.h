8c3eed2260bd3355b3aa989f722091b9cbf176f2
Raíssa Santos <raixapsantos@gmail.com>
2021-06-27 23:09:48 -0300

    Terminada

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -11,22 +11,70 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt1 Iterator para o range.
+ * @tparam InputIt2 Iterator para o range.
+ * @tparam Equal Tipo de uma função bool eq(const string &a,  const string &b)
+ * 
+ * @param first1 Ponteiro para o primeiro elemento do range.
+ * @param last1 Ponteiro para a posição logo após o último elemento do range.
+ * @param first2 Ponteiro para o primeiro elemento do range.
+ * @param eq Função para comparar dois valores.
+ * @param it Ponteiro para percorrer o range.
+ * @param count Armazenar a quantidade de elementos que testaram falso para p.
+ * 
+ * @return Verdadeiro se os ranges são iguais, falso caso contrário.
  */
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
 {
-    // TODO
-    return false;
+    int count = 0;
+
+    for(InputIt1 it = first1; it != last1; ++it)
+    {
+        if(!eq(*it,*first2))
+            count++;
+        first2++;
+    }
+
+    if(count > 0)
+        return false;
+    else
+        return true;
 }
 
+/*! 
+ * @tparam InputIt1 Iterator para o range.
+ * @tparam InputIt2 Iterator para o range.
+ * @tparam Equal Tipo de uma função bool eq(const string &a,  const string &b)
+ * 
+ * @param first1 Ponteiro para o primeiro elemento do range.
+ * @param last1 Ponteiro para a posição logo após o último elemento do range.
+ * @param first2 Ponteiro para o primeiro elemento do range.
+ * @param last2 Ponteiro para a posição logo após o último elemento do range.
+ * @param eq Função para comparar dois valores.
+ * @param it Ponteiro para percorrer o range.
+ * @param count Armazenar a quantidade de elementos que testaram falso para p.
+ * 
+ * @return Verdadeiro se os ranges são iguais, falso caso contrário.
+ */
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1,
            InputIt2 first2, InputIt2 last2,
            Equal eq )
 {
-    // TODO
-    return false;
+    int count = 0;
+
+    for(InputIt1 it = first1; it != last1; it++)
+    {
+        if(!eq(*it,*first2))
+            count++;
+        first2++;
+    }
+
+    if(count > 0)
+        return false;
+    else
+        return true;
 }
 
 }

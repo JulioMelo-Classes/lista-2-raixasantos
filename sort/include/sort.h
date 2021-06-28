de781af4b33d56a6c24a877966d87b3d1fd085d5
Raíssa Santos <raixapsantos@gmail.com>
2021-06-27 23:20:44 -0300

    Terminada

--- sort/include/sort.h
+++ sort/include/sort.h
@@ -11,12 +11,28 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam ForwardIt Iterator para o range.
+ * @tparam Comparison Tipo de uma função bool cmp( const Type &a, const Type &b).
+ * 
+ * @param first Ponteiro para o primeiro elemento do range.
+ * @param last Ponteiro para a posição logo após o último elemento do range.
+ * @param cmp Função para comparar dois valores.
+ * @param it1 Ponteiro para percorrer o range.
+ * @param it2 Ponteiro para percorrer o range.
+ * 
+ * @return void
  */
 template<class ForwardIt, class Comparison>
 void sort(ForwardIt first, ForwardIt last, Comparison cmp)
 {
-    // TODO
+    for(ForwardIt it1 = first; it1 != last; it1++)
+    {
+        for(ForwardIt it2 = it1; it2 != last; it2++)
+        {
+            if(cmp(*it2,*it1))
+                std::swap(*it1,*it2);
+        }
+    }
 }
 
 }
